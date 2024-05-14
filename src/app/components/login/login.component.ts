5c9a457e08583a015fb05f4b41b72b9ade5c71ae
CristianoMends <mendescristiano012@gmail.com>
2024-05-14 12:03:15 -0300

    updated design

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,38 +7,48 @@ import { MessageDialogComponent } from '../message-dialog/message-dialog.compone
 import { User } from '../../interface/user.interface';
 
 @Component({
-    selector: 'app-login',
-    standalone: true,
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css',
-    imports: [CommonModule, FormsModule, MessageDialogComponent]
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  imports: [CommonModule, FormsModule, MessageDialogComponent]
 })
 export class LoginComponent {
 
   constructor(
     private userService: UserService
   ) {
-    
+
     this.isLoggedIn();
 
   }
 
+  hidePassword = true;
+  hideConfirmPassword = true;
   private auth: Auth = { email: '', password: '' };
   private token: string = '';
+
+  toggleVisibility(field: string) {
+    if (field === 'password') {
+      this.hidePassword = !this.hidePassword;
+    } else if (field === 'confirm') {
+      this.hideConfirmPassword = !this.hideConfirmPassword;
+    }
+  }
 
   getAuth(): Auth {
     return this.auth;
   }
 
-  isLoggedIn(){
+  isLoggedIn() {
     this.userService.validateToken()
     const token = this.userService.getToken();
-    if (token != null) { 
-      MessageDialogComponent.showMessage('Bem vindo de volta!',`Redirecionando para seu perfil`);
-      setTimeout(function(){
+    if (token != null) {
+      MessageDialogComponent.showMessage('Bem vindo de volta!', `Redirecionando para seu perfil`);
+      setTimeout(function () {
         window.location.href = 'profile';
-      },1500);
-     };
+      }, 1500);
+    };
   }
 
   onSubmit() {
@@ -48,6 +58,6 @@ export class LoginComponent {
     });
   }
   onFailedLogin() {
-    MessageDialogComponent.showMessage('Usuário não encontrado',`Verifique seu email e senha e tente novamente, ou crie sua conta!`,()=> {});
+    MessageDialogComponent.showMessage('Usuário não encontrado', `Verifique seu email e senha e tente novamente, ou crie sua conta!`, () => { });
   }
 }
